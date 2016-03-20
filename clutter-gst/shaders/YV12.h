88e58b0bc401aa7a72cfb27239aaa8d360e7986d
Lionel Landwerlin <llandwerlin@gmail.com>
2016-03-20 16:21:47 +0000

    shaders: modify cg shaders rather than generated files

--- clutter-gst/shaders/YV12.h
+++ clutter-gst/shaders/YV12.h
@@ -9,27 +9,27 @@
  * This define is the size of the shader in bytes.  More precisely it's the
  * sum of strlen() of every string in the array.
  */
-#define YV12_FP_SZ    572
+#define YV12_FP_SZ    564
 
 static const char *YV12_fp[] =
 {
   "!!ARBfp1.0\n",
-  "PARAM c[2] = { { 1.1640625, 0.5, 2.015625, 0.0625 },\n",
-  "{ 0.390625, 0, 1.5976562, 0.8125 } };\n",
+  "PARAM c[2] = { { 1.1640625, 0.0625, 2.015625, 0.5 },\n",
+  "{ 0.390625, 0.8125, 1.5976562 } };\n",
   "TEMP R0;\n",
   "TEMP R1;\n",
   "TEX R1.w, fragment.texcoord[0], texture[0], 2D;\n",
-  "ADD R0.x, R1.w, -c[0].w;\n",
+  "ADD R0.x, R1.w, -c[0].y;\n",
   "TEX R0.w, fragment.texcoord[0], texture[1], 2D;\n",
   "TEX R1.w, fragment.texcoord[0], texture[2], 2D;\n",
   "MUL R0.x, R0, c[0];\n",
-  "ADD R0.y, R1.w, -c[0];\n",
+  "ADD R0.y, R1.w, -c[0].w;\n",
   "MAD R0.z, R0.y, c[0], R0.x;\n",
-  "ADD R0.w, R0, -c[0].y;\n",
   "MAD R0.y, -R0, c[1].x, R0.x;\n",
+  "ADD R0.w, R0, -c[0];\n",
   "MAD R0.x, R0.w, c[1].z, R0;\n",
-  "MAD R0.w, -R0, c[1], R0.y;\n",
-  "MUL result.color.xyz, fragment.color.primary.w, R0.xwzy;\n",
+  "MAD R0.y, -R0.w, c[1], R0;\n",
+  "MUL result.color.xyz, fragment.color.primary.w, R0;\n",
   "MOV result.color.w, fragment.color.primary;\n",
   "END\n",
   NULL
